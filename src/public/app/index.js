cef333a2acb373ee33df2f5ead9344442d01c16f
benjamin wilson <wilsob12@gamil.com>
2016-05-22 16:49:08 -0400

    Init
    Init commit

---
+++ src/public/app/index.js
@@ -0,0 +1,6 @@
+"use strict";
+var environment_1 = require('./environment');
+exports.environment = environment_1.environment;
+var test_project_component_1 = require('./test-project.component');
+exports.TESTPROJECTAppComponent = test_project_component_1.TESTPROJECTAppComponent;
+//# sourceMappingURL=index.js.map
---
+++ src/public/app/index.ts
@@ -0,0 +1,2 @@
+export {environment} from './environment';
+export {TESTPROJECTAppComponent} from './test-project.component';
